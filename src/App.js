db865599d16779db5bfd8c9182b81203bc122d86
Abhishek <hahaguyz546@gmail.com>
2023-06-21 00:25:05 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import MovieCard from "./Movie";
+
+const API_URL = "https://omdbapi.com?apikey=f2bb7370";
+const movie1 = {
+  Title: "Batman v Superman: Dawn of Justice",
+  Year: "2016",
+  imdbID: "tt2975590",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+};
 
 function App() {
+  const [movie, setMovie] = useState([]);
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+    setMovie(data.Search);
+  };
+  function hitKey(e) {
+    if (e.keyCode != 13) return;
+    searchMovie(e.target.value);
+  }
+
+  // useEffect(() => {
+  //   searchMovie("Memento");
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="search-bar">
+        <h1>Movie Hub</h1>
+        <input className="search" type="search" onKeyDown={hitKey} />
+      </div>
+      <div className="container">
+        {movie ? (
+          movie.map(function (mv) {
+            return <MovieCard movie={mv} />;
+          })
+        ) : (
+          <div className="error">No Movie Found</div>
+        )}
+      </div>
+    </>
   );
 }
-
 export default App;
